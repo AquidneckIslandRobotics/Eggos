bdec58a7489ccb736db2da8bb017c8a64bd58f59
siebs7317 <35415103+siebs7317@users.noreply.github.com>
2020-02-27 09:04:53 -0500

    Update Chassis.java
    Added pigeon code to drive straight

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -16,6 +16,7 @@ import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.sensors.PigeonIMU_StatusFrame;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
@@ -26,7 +27,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Chassis extends SubsystemBase {
-  private PigeonIMU pidgey = new PigeonIMU(20);
+  private PigeonIMU pidgey;
   private TalonFX leftLead;
   private BaseMotorController leftFollow;
   private TalonFX rightLead;
@@ -49,6 +50,7 @@ public class Chassis extends SubsystemBase {
     leftFollow = new TalonFX(15);
     rightLead = new TalonFX(1);
     rightFollow = new TalonFX(16);
+    pidgey = new PigeonIMU(20)
 
     leftFollow.configFactoryDefault();
     leftFollow.follow(leftLead);
@@ -57,13 +59,14 @@ public class Chassis extends SubsystemBase {
 
     leftLead.configFactoryDefault();
     rightLead.configFactoryDefault();
+    pidgey.configFactoryDefault();
 
-    leftLead.setInverted(true);
+    leftLead.setInverted(TalonFXInvertType.Clockwise);
     leftLead.setSensorPhase(false);
-    leftFollow.setInverted(true);
-    rightLead.setInverted(false);
+    leftFollow.setInverted(TalonFXInvertType.Clockwise);
+    rightLead.setInverted(TalonFXInvertType.CounterClockwise);
     rightLead.setSensorPhase(false);
-    rightFollow.setInverted(false);
+    rightFollow.setInverted(TalonFXInvertType.CounterClockwise);
 
     leftLead.configOpenloopRamp(0.8);
     leftFollow.configOpenloopRamp(0.8);
@@ -71,14 +74,17 @@ public class Chassis extends SubsystemBase {
     rightFollow.configOpenloopRamp(0.8);
 
 
+    leftLead.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 20, 30);
+    leftLead.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 20, 30);
+    leftLead.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 5, 30);
 
-    leftLead.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
-    leftLead.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 30);
-
-    rightLead.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
-    rightLead.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 30);
+    rightLead.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 20, 30);
+    rightLead.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 20, 30);
+    rightLead.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 20, 30);
+    rightLead.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 20, 30);
+    rightLead.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10, 30);
   
-    //pidgey.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR, 05, 30);
+    pidgey.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR, 05, 30);
 
     _motion_magic.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
     _motion_magic.neutralDeadband = 0.001;
@@ -96,18 +102,24 @@ public class Chassis extends SubsystemBase {
     _leftConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; 
     _rightConfig.remoteFilter0.remoteSensorDeviceID = leftLead.getDeviceID(); 
     _rightConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.TalonFX_SelectedSensor; 
-    pidgey.configFactoryDefault();
+    
+    // Setup Left (Aux) and Right (Master)
+    _rightConfig.sum0Term = FeedbackDevice.RemoteSensor0;    //Aux Selected Sensor
+    _rightConfig.sum1Term = FeedbackDevice.IntegratedSensor; //Local IntegratedSensor
+    _rightConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.SensorSum; //Sum0 + Sum1
+    _rightConfig.primaryPID.selectedFeedbackCoefficient = 0.5;
 
-   // setRobotConfigs(_rightInvert, _rightConfig); 
+    // setRobotConfigs(_rightInvert, _rightConfig); 
     _rightConfig.slot0.kF = Constants.kGains_Distanc.kF; 
     _rightConfig.slot0.kP = Constants.kGains_Distanc.kP;
-		_rightConfig.slot0.kI = Constants.kGains_Distanc.kI;
-		_rightConfig.slot0.kD = Constants.kGains_Distanc.kD;
-		_rightConfig.slot0.integralZone = Constants.kGains_Distanc.kIzone;
+    _rightConfig.slot0.kI = Constants.kGains_Distanc.kI;
+    _rightConfig.slot0.kD = Constants.kGains_Distanc.kD;
+    _rightConfig.slot0.integralZone = Constants.kGains_Distanc.kIzone;
     _rightConfig.slot0.closedLoopPeakOutput = Constants.kGains_Distanc.kPeakOutput;
+    // Heading Config
     _rightConfig.remoteFilter1.remoteSensorDeviceID = pidgey.getDeviceID();    //Pigeon Device ID
-	  _rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.Pigeon_Yaw; //This is for a Pigeon over CAN
-	  _rightConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor1; //Set as the Aux Sensor
+    _rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.Pigeon_Yaw; //This is for a Pigeon over CAN
+    _rightConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor1; //Set as the Aux Sensor
     _rightConfig.auxiliaryPID.selectedFeedbackCoefficient = 3600.0 / Constants.kPigeonUnitsPerRotation;
 
     _rightConfig.auxPIDPolarity = false;
@@ -121,15 +133,16 @@ public class Chassis extends SubsystemBase {
 
     _leftConfig.neutralDeadband = Constants.kNeutralDeadband;
     _rightConfig.neutralDeadband = Constants.kNeutralDeadband;
+	  
+    int closedLoopTimeMs = 1;
+    rightLead.configClosedLoopPeriod(0, closedLoopTimeMs, 30);
+    rightLead.configClosedLoopPeriod(1, closedLoopTimeMs, 30);
 
     _rightConfig.motionAcceleration = 6000; //(distance units per 100 ms) per second
     _rightConfig.motionCruiseVelocity = 15000; //distance units per 100 ms
-    
-    _leftConfig.motionAcceleration = 6000; //(distance units per 100 ms) per second
-		_leftConfig.motionCruiseVelocity = 15000; //distance units per 100 ms
 
     leftLead.configAllSettings(_leftConfig);
-		rightLead.configAllSettings(_rightConfig); 
+    rightLead.configAllSettings(_rightConfig); 
 	  
 	  
     // Music
@@ -213,9 +226,17 @@ public void stopDriveMotors() {
   }
 
   public void resetEncoder(){
-  leftLead.getSensorCollection().setIntegratedSensorPosition(0, 30); 
-  rightLead.getSensorCollection().setIntegratedSensorPosition(0, 30); 
- }
+    leftLead.getSensorCollection().setIntegratedSensorPosition(0, 30); 
+    rightLead.getSensorCollection().setIntegratedSensorPosition(0, 30); 
+  }
+	
+  public void zeroAllSensors() {
+    leftLead.getSensorCollection().setIntegratedSensorPosition(0, 30); 
+    rightLead.getSensorCollection().setIntegratedSensorPosition(0, 30); 
+    pidgey.setYaw(0, 30);
+    pidgey.setAccumZAngle(0, 30);
+  }
+	
   public void switchDirection(){
    
     leftLead.setInverted(!leftLead.getInverted());
